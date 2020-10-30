fcf34460c5566b6adb0e886bc19cf99b4a1bbd8a
dramastore <georgehmariki@gmail.com>
2020-10-30 23:12:42 +0300

    New Drama Added

--- script.js
+++ script.js
@@ -104,6 +104,331 @@ else if (input == 1776) {
     downloadButton.setAttribute('href', "https://tinyurl.com/penthouse-war")
 }
 
+else if (input == 1777) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: More Than Friends"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/More-Than-Friend")
+}
+
+else if (input == 1778) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Delayed Justice"
+    episodes.innerHTML = '<b>Episodes:</b> 20'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Delayed-Justice-TG")
+}
+
+else if (input == 1779) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Start-Up"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Start-Up-TG")
+}
+
+else if (input == 1780) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Search (2020)"
+    episodes.innerHTML = '<b>Episodes:</b> 10'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Search-TG")
+}
+
+else if (input == 1781) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Homemade Lovestory"
+    episodes.innerHTML = '<b>Episodes:</b> 100'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Homemade-Lovestory-TG")
+}
+
+else if (input == 1782) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Lies Of Lies"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Lies-Of-Lies-TG")
+}
+
+else if (input == 1783) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: ALICE"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Download-Alice-TG")
+}
+
+else if (input == 1784) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Twenty-Twenty"
+    episodes.innerHTML = '<b>Episodes:</b> 20'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Twenty-Twenty-TG")
+}
+
+else if (input == 1785) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Reply 1988"
+    episodes.innerHTML = '<b>Episodes:</b> 20'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Reply-1988-TG")
+    document.getElementById('year').innerHTML = '<b>Year:</b> 2015'
+}
+
+else if (input == 1786) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Missing: The Other Side"
+    episodes.innerHTML = '<b>Episodes:</b> 12'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Missing-The-Other-Side-TG")
+}
+
+else if (input == 1787) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: SF8"
+    episodes.innerHTML = '<b>Episodes:</b> 08'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Download-SF8-TG")
+}
+
+else if (input == 1788) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Stranger: Season 1"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Stranger-1-TG")
+    document.getElementById('year').innerHTML = '<b>Year:</b> 2017'
+}
+
+else if (input == 1789) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Stranger: Season 2"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Sranger-2-TG")
+}
+
+else if (input == 1790) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: The School Nurse Files"
+    episodes.innerHTML = '<b>Episodes:</b> 06'
+    downloadButton.setAttribute('href', "https://tinyurl.com/The-School-Nurse-Files-TG")
+}
+
+else if (input == 1791) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: It's Okay To Not Be Okay"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Its-okay-to-not-be-okay-TG")
+}
+
+else if (input == 1792) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Mr. Heart"
+    episodes.innerHTML = '<b>Episodes:</b> 08'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Mr-Heart-TG")
+}
+
+else if (input == 1793) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Were Your Eyes Linger"
+    episodes.innerHTML = '<b>Episodes:</b> 08'
+    downloadButton.setAttribute('href', "https://tinyurl.com/were-your-eyes-linger-TG")
+}
+
+else if (input == 1794) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Crash Landing on You"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Crash-Landing-On-You-TG")
+}
+
+else if (input == 1795) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Flower Of Evil"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Flower-Of-Evil-TG")
+}
+
+else if (input == 1796) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Once Again"
+    episodes.innerHTML = '<b>Episodes:</b> 100'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Once-Again-TG")
+}
+
+else if (input == 1797) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Graceful Friends"
+    episodes.innerHTML = '<b>Episodes:</b> 17'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Graceful-Friends-TG")
+}
+
+else if (input == 1798) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Was It Love"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Was-It-Love-TG")
+}
+
+else if (input == 1799) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Men Are Men"
+    episodes.innerHTML = '<b>Episodes:</b> 32'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Men-Are-Men-TG")
+}
+
+else if (input == 1800) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: CHIP-IN"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Chip-In-TG")
+}
+
+else if (input == 1801) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Train"
+    episodes.innerHTML = '<b>Episodes:</b> 12'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Train-TG")
+}
+
+else if (input == 1802) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: She Knows Everything"
+    episodes.innerHTML = '<b>Episodes:</b> 08'
+    downloadButton.setAttribute('href', "https://tinyurl.com/She-Knows-Everything-TG")
+}
+
+else if (input == 1803) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: The Good Detective"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/The-Good-Detective-TG")
+}
+
+else if (input == 1804) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Into The Ring / Memorials"
+    episodes.innerHTML = '<b>Episodes:</b> 32'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Memorials-TG")
+}
+
+else if (input == 1805) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Pop-Out Boy"
+    episodes.innerHTML = '<b>Episodes:</b> 10'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Pop-Out-Boy-TG")
+}
+
+else if (input == 1806) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Backstreet Rookie"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Backstreet-Rookie-TG")
+}
+
+else if (input == 1807) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: My Unfamiliar Family"
+    episodes.innerHTML = '<b>Episodes:</b> 16'
+    downloadButton.setAttribute('href', "https://tinyurl.com/My-Unfamiliar-Family-TG")
+}
+
+else if (input == 1808) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Sweet Munchies"
+    episodes.innerHTML = '<b>Episodes:</b> 12'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Sweet-Munchies-TG")
+}
+
+else if (input == 1809) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Dinner Mate"
+    episodes.innerHTML = '<b>Episodes:</b> 32'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Dinner-Mate-TG")
+}
+
+else if (input == 1810) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Team Bulldog: Off-Duty Investigation"
+    episodes.innerHTML = '<b>Episodes:</b> 12'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Team-Bulldog-TG")
+}
+
+else if (input == 1811) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: King Maker: The Change Of Destiny"
+    episodes.innerHTML = '<b>Episodes:</b> 21'
+    downloadButton.setAttribute('href', "https://tinyurl.com/King-Maker-TG")
+}
+
+else if (input == 1812) {
+    displayMessage.style.display = 'block'
+    overlay.style.display = 'block'
+
+    profile.innerText = "Drama: Mystic Pop Up Bar"
+    episodes.innerHTML = '<b>Episodes:</b> 12'
+    downloadButton.setAttribute('href', "https://tinyurl.com/Mystic-Pop-Up-Bar-TG")
+}
     else {
         let ujumbe = document.getElementById('ujumbe')
         ujumbe.style.display = 'block';
