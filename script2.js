2a07ea3395f3fec0bf0148ac469b2793b6e92699
dramastore <georgehmariki@gmail.com>
2020-11-23 04:29:47 +0300

    website content changed

--- script2.js
+++ script2.js
@@ -1,25 +1,3 @@
-function changingCode() {
-    var ujumbe1 = document.getElementById('ujumbe')
-    var checking = ujumbe1.style.display
-    if (checking == 'block') {
-        ujumbe1.style.display = 'none'
-    }
-}
-
-
-
-
-
-
-function closePop() {
-    let displayMessage = document.getElementById('display-download')
-    let overlay = document.getElementById('overlay')
-    overlay.style.display = 'none'
-    displayMessage.style.display = 'none'
-}
-
-
-
 var tarehe = new Date();
 var mwaka = tarehe.getFullYear()
 var mwezi = tarehe.getMonth() + 1
