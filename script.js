095c43766ed9a74fb912eee8ac559df16b9a3659
dramastore <64647629+dramastore@users.noreply.github.com>
2020-12-20 23:09:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ function searching() {
         }
     })
 }
+
+function openAd() {
+    setTimeout(()=>{
+       window.open('http://zuphaims.com/afu.php?zoneid=3800334')
+    }, 60000)
+}
+
+openAd();
